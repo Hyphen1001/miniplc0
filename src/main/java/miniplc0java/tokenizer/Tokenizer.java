42cca6993b2c95f7115bb345411a37b4c8f90cf2
Hyphen1001 <1415521217@qq.com>
2020-10-22 16:35:59 +0800

    词法分析

--- src/main/java/miniplc0java/tokenizer/Tokenizer.java
+++ src/main/java/miniplc0java/tokenizer/Tokenizer.java
@@ -45,15 +45,7 @@ public class Tokenizer {
         }
 
         return new Token(TokenType.Uint,tmp,it.previousPos(), it.currentPos());
-        // 请填空：
-            // 直到查看下一个字符不是数字为止:
-        // -- 前进一个字符，并存储这个字符
-        //
-        // 解析存储的字符串为无符号整数
-        // 解析成功则返回无符号整数类型的token，否则返回编译错误
-        //
-        // Token 的 Value 应填写数字的值
-        //throw new Error("Not implemented");
+
     }
 
     private Token lexIdentOrKeyword() throws TokenizeError {
@@ -75,16 +67,7 @@ public class Tokenizer {
             default:
                 return new Token(TokenType.Ident, tmp, it.previousPos(), it.currentPos());
         }
-        // 请填空：
-        // 直到查看下一个字符不是数字或字母为止:
-        // -- 前进一个字符，并存储这个字符
-        //
-        // 尝试将存储的字符串解释为关键字
-        // -- 如果是关键字，则返回关键字类型的 token
-        // -- 否则，返回标识符
-        //
-        // Token 的 Value 应填写标识符或关键字的字符串
-        //throw new Error("Not implemented");
+
     }
 
     private Token lexOperatorOrUnknown() throws TokenizeError {
@@ -93,36 +76,36 @@ public class Tokenizer {
                 return new Token(TokenType.Plus, '+', it.previousPos(), it.currentPos());
 
             case '-':
-                // 填入返回语句
+
                 return new Token(TokenType.Minus, '-', it.previousPos(), it.currentPos());
 
             case '*':
-                // 填入返回语句
+
                 return new Token(TokenType.Mult, '*', it.previousPos(), it.currentPos());
 
             case '/':
-                // 填入返回语句
+
                 return new Token(TokenType.Div, '/', it.previousPos(), it.currentPos());
 
             case '=':
-                // 填入返回语句
+
                 return new Token(TokenType.Equal, '=', it.previousPos(), it.currentPos());
 
             case ';':
-                // 填入返回语句
+
                 return new Token(TokenType.Semicolon, ';', it.previousPos(), it.currentPos());
 
             case '(':
-                // 填入返回语句
+
                 return new Token(TokenType.LParen, '(', it.previousPos(), it.currentPos());
 
             case ')':
-                // 填入返回语句
+
                 return new Token(TokenType.RParen, ')', it.previousPos(), it.currentPos());
-                // 填入更多状态和返回语句
+
 
             default:
-                // 不认识这个输入，摸了
+
                 throw new TokenizeError(ErrorCode.InvalidInput, it.previousPos());
         }
     }
